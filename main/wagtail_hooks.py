3552ee013db5e0ee1e37e479d975d3165d629d31
Camille Legeron <camille@telescoop.fr>
2022-03-18 16:29:43 +0000

    [+] Free body and contentPage

--- main/wagtail_hooks.py
+++ main/wagtail_hooks.py
@@ -1,10 +1,21 @@
+from django.templatetags.static import static
+from django.utils.html import format_html
 from wagtail.contrib.modeladmin.options import (
     ModelAdmin,
     ModelAdminGroup,
     modeladmin_register,
 )
+from wagtail.core import hooks
 
 from main.models import News, Ressource, Thematic, ActualityType, Profile
+
+
+@hooks.register("insert_global_admin_css", order=100)
+def global_admin_css():
+    """Add /static/css/geodev-admin.css to the admin."""
+    return format_html(
+        '<link rel="stylesheet" href="{}">', static("css/geodev-admin.css")
+    )
 
 
 class ProfileModelAdmin(ModelAdmin):
