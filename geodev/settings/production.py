59e2b9cf1e82cc12b380c8782d3c42a0ad3aa1be
Maxime Bellec <maxbellec@gmail.com>
2022-03-22 15:02:07 +0100

    [*] add vault and rollbar

--- geodev/settings/production.py
+++ geodev/settings/production.py
@@ -6,3 +6,16 @@ try:
     from .local import *  # noqa: F401,F403
 except ImportError:
     pass
+
+MIDDLEWARE.append(  # noqa: F405
+    "rollbar.contrib.django.middleware.RollbarNotifierMiddleware"
+)
+
+ROLLBAR = {
+    "access_token": "43c4b59af5d14116a7077bc2612df1e0",
+    "environment": "development" if DEBUG else "production",
+    "root": BASE_DIR,  # noqa: F405
+}
+import rollbar  # noqa: E402
+
+rollbar.init(**ROLLBAR)
